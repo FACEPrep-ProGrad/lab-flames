b987330e20de407dc48851379842a82dc4d644ea
Subham Das <subhamdas226@gmail.com>
2020-04-28 23:23:44 +0530

    FSJ0320B002

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -6,4 +6,80 @@ package service;
 // Your task is to calculate the flames value and return the corresponding character as output 
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
-
+public class FlamesCheckService{
+	public char findFlames(String name1, String name2) {
+		
+		StringBuilder n1 =new StringBuilder(name1);
+		StringBuilder n2 = new StringBuilder(name2);
+		
+		//char arr[] = {'f','l','a','m','e','s'};
+		int sn1 =n1.length();
+		int sn2 =n2.length();
+		int count = n1.length() + n2.length();
+		System.out.println(count);
+		
+		for(int i=0; i<sn1; i++) {
+			for(int j=0; j<sn2;j++) {
+				if(n1.charAt(i) == n2.charAt(j)) {
+					count = count - 2;
+					n1.deleteCharAt(i);
+					n2.deleteCharAt(j);
+					sn1 = n1.length();	
+					sn2 = n2.length();
+					i=0;
+					j=0;
+				}
+			}
+		}
+		
+		String f = "flames";
+		char ch;
+		int x=0;
+		StringBuffer flames = new StringBuffer(f);
+		if(count > flames.length()) {
+			 x = count % flames.length();
+			 x=x-1;
+			 ch =f.charAt(x);
+		}
+		else if(count == flames.length()) {
+			int c = count-1;
+			ch = f.charAt(c);
+		}
+		else {
+			 ch =f.charAt(count-1);
+		}
+		System.out.println("count :"+count);
+		System.out.println("Your name is "+name1);
+		System.out.println("Your Partner name is "+name2);
+		System.out.println(ch);
+		
+		switch(ch)
+		{
+			case 'f':
+				System.out.println("You Two are Friends");
+				break;
+		
+		
+			case 'l':
+				System.out.println("You Two are Love");
+				break;
+				
+			case 'a':
+				System.out.println("You Two are Affection");
+				break;
+				
+			case 'm':
+				System.out.println("You Two are Marriage");
+				break;
+				
+			case 'e':
+				System.out.println("You Two are Enemy");
+					break;
+				
+			case 's':
+				System.out.println("You Two are Siblings");
+				break;			
+		}				
+		return ch;
+	}
+}
