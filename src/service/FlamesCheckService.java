76e1119b46b74ad6be859e45e8aa93f509e9be55
3deep0019 <trideep@LAPTOP-U7JART73.localdomain>
2020-09-09 13:50:39 +0530

    FSJ0820B106

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -7,3 +7,44 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+public class FlamesCheckService {
+	public static int removeCharacter(String name_1,String name_2) {
+		StringBuilder n1 = new StringBuilder(name_1);
+		StringBuilder n2 = new StringBuilder(name_2);
+		
+		for(int i=0;i<n1.length();i++) {
+			for(int j=0;j<n2.length();j++) {
+				if(n1.charAt(i)==n2.charAt(j)) {
+					n1.deleteCharAt(i);
+					n2.deleteCharAt(j);
+					i = 0;
+					j=0;
+				}
+			}
+		}
+		int totalLength = n1.length() + n2.length();
+		return totalLength;
+	}
+	
+	public char Flames(String name_1,String name_2) {
+		StringBuilder flame = new StringBuilder("flames");
+		int index,flamesIndex;
+		flamesIndex = removeCharacter(name_1,name_2);
+		flamesIndex = flamesIndex-1;
+		
+		while(flame.length()!=1) {
+			if (flamesIndex >= 6)
+				index = flamesIndex % flame.length();
+			else
+				index = flamesIndex;
+
+			flame.deleteCharAt(index);
+			StringBuilder newFlame = new StringBuilder((flame.substring(index) + flame.substring(0, index)));
+			flame = newFlame;
+		}
+		System.out.println(flame.charAt(0));
+		return flame.charAt(0);
+		
+	}
+	
+}
